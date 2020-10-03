fcff1a47cef1b02a4233cd16ffdaa50c6df58aea
Roman-Denysenko <roman.denisenko83@gmail.com>
2020-10-03 23:57:37 +0300

    Решает 5 и 6  задачи.

--- js/task-5.js
+++ js/task-5.js
@@ -5,3 +5,17 @@
 
 // <input type="text" placeholder="Ваше имя?" id="name-input" />
 // <h1>Привет, <span id="name-output">незнакомец</span>!</h1>
+
+const inputRef = document.querySelector(`#name-input`);
+const nameRef = document.querySelector(`#name-output`);
+
+inputRef.addEventListener(`input`, onWritesName);
+
+function onWritesName(event) {
+  const text = `незнакомец`;
+  let inputValue = event.currentTarget.value;
+
+  event.data === null
+    ? (nameRef.textContent = text)
+    : (nameRef.textContent = inputValue);
+}
